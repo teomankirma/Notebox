f3f5d6232a8910b53bdfea467e7c914d50b9444e
Teoman Kırma <83593653+teomankirma@users.noreply.github.com>
2023-06-01 17:10:52 +0300

    feat: starter files

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Notebox.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
