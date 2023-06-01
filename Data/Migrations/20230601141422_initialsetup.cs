e3b6bea5410712938c2d709af39f19cd9ada8ddf
Teoman Kırma <83593653+teomankirma@users.noreply.github.com>
2023-06-01 17:19:40 +0300

    feat: note model and controllers for actions

---
+++ Data/Migrations/20230601141422_initialsetup.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Notebox.Data.Migrations
+{
+    public partial class initialsetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Note",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NoteTitle = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NoteContent = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Note", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Note");
+        }
+    }
+}
---
+++ Models/Note.cs
@@ -0,0 +1,15 @@
+﻿namespace Notebox.Models
+{
+    public class Note
+    {
+        public string UserId { get; set; }
+        public int Id { get; set; }
+        public string NoteTitle { get; set; }
+        public string NoteContent { get; set; }
+
+        public Note()
+        {
+
+        }
+    }
+}
