1670df794c930e0fbb7e868e8565c5e9479ba796
Teoman Kırma <83593653+teomankirma@users.noreply.github.com>
2023-06-03 16:59:09 +0300

    feat: animations by using jQuery

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,21 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿
+// FadeIn effects for views by using jQuery
+$(document).ready(function () {
+    $(".welcome-screen").fadeIn(1500).removeClass("hidden");
+    $(".login").fadeIn(1500).removeClass("hidden");
+    $(".register").fadeIn(1500).removeClass("hidden");
+    $(".resend-email").fadeIn(1500).removeClass("hidden");
+    $(".notes").fadeIn(1500).removeClass("hidden");
+    $(".create").fadeIn(1500).removeClass("hidden");
+    $(".edit").fadeIn(1500).removeClass("hidden");
+    $(".details").fadeIn(1500).removeClass("hidden");
+    $(".delete").fadeIn(1500).removeClass("hidden");
+    $(".search").fadeIn(1500).removeClass("hidden");
+    $(".manage").fadeIn(1500).removeClass("hidden");
+    $(".email").fadeIn(1500).removeClass("hidden");
+    $(".password").fadeIn(1500).removeClass("hidden");
+    $(".personal").fadeIn(1500).removeClass("hidden");
+    $(".delete-personal").fadeIn(1500).removeClass("hidden");
+    $(".register-confirmation").fadeIn(1500).removeClass("hidden");
+});
 
-// Write your JavaScript code.
